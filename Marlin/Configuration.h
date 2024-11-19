ec6ea48ca5f40abaaf3010f0191d9d71c100744e
Adam Purdie <adam@symmetry.ninja>
2024-11-19 21:28:59 +1100

    renaming to spot

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -149,7 +149,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Spike3d 1.4"
+#define CUSTOM_MACHINE_NAME "Spot"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
