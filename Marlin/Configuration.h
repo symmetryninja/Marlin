78bd12d913fe0664762f199bb3caf09bff3b2959
spidey <adam@symmetry.ninja>
2024-11-23 18:13:29 +1100

    spot-tweaks

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1882,12 +1882,12 @@
 #define Y_BED_SIZE 250 // changed for spot
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
-#define X_MIN_POS 0 //TODO
-#define Y_MIN_POS 0 //TODO
+#define X_MIN_POS -24
+#define Y_MIN_POS -17
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 300 // changed for spot
+#define Z_MAX_POS 300 // changed for spot 
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
 //#define J_MIN_POS 0
